940c28199c09931ca57521cdaf0b04bbc69583f1
dylan <0kymmx@gmail.com>
2014-05-09 09:59:13 +0900

    최근에 추가된 번역

---
+++ app/lang/ko/admin/groups/titles.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+
+	'group_management' 	 	=> '그룹 관리',
+	'create_group' 	 		=> '그룹 등록',
+	'edit_group' 	 		=> '그룹 편집',
+	'group_name' 	 		=> '그룹 명',
+	'group_admin' 	 		=> '그룹 관리자',
+	'allow' 	 			=> '허가',
+	'deny' 	 				=> '불가',
+
+);
